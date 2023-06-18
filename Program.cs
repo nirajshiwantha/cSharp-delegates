5f6060b22dee778a8d56e41c76152e572e62a2a3
niraj_ranasinghe <niraj.ranasinghe@covalentworld.com>
2023-06-18 21:31:25 +0530

    initial commit with complete implementation

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿class Calculator
+{
+    public static void Add(int a, int b)
+    {
+        int result = a + b;
+        Console.WriteLine($"The sum is: {result}");
+    }
+
+    public static void Mult(int a, int b)
+    {
+        int result = a * b;
+        Console.WriteLine($"The product is: {result}");
+    }
+}
+
+//class Program
+//{
+//    delegate void CalculatorDelegate(int a, int b);
+
+//    static void Main(string[] args)
+//    {
+//        CalculatorDelegate calculator = Calculator.Add;
+//        calculator(10, 6); // Output: The sum is: 15
+
+//        calculator = Calculator.Mult;
+//        calculator(10, 5); // Output: The product is: 50
+//    }
+//}
+
+//class Program
+//{
+//    delegate void CalculatorDelegate(int a, int b);
+
+//    static void Main(string[] args)
+//    {
+//        CalculatorDelegate calculator = Calculator.Add;
+//        calculator += Calculator.Mult;
+//        calculator(10, 5); // Output: The sum is: 15, The product is: 50
+//    }
+//}
+
+class Programs
+{
+    delegate void AnonymousDelegate(int a, int b);
+
+    static void Main(string[] args)
+    {
+        AnonymousDelegate calculator = delegate (int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine($"The sum is: {result}");
+        };
+
+        calculator(10, 5); // Output: The sum is: 15
+    }
+}
